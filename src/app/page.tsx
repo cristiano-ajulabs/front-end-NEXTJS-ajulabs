0a4f0100c60d1e3a5f6581bb5466bb2fb565741f
Cristiano <cristiano@ajulabs.com>
2025-06-16 16:20:13 -0300

    Fron-end-NEXTJS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,80 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client"
+import { useEffect, useState, useRef } from 'react'
+import api from './services/api'
+import { get } from 'http'
+
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  type Usuario = {
+    id: string
+    name: string
+    email: string
+  }
+  const [usus, setUsus] = useState<Usuario[]>([])
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  const inputName = useRef<HTMLInputElement>(null)
+  const inputEmail = useRef<HTMLInputElement>(null)
+  const inputPassword= useRef<HTMLInputElement>(null)
+
+  async function getUsers() {
+    const response = await api.get('/users')
+    setUsus(response.data)
+  }
+
+  async function createUsers() {
+    if (
+      inputName.current &&
+      inputEmail.current &&
+      inputPassword.current
+    ) {
+    await api.post("/register", {
+      name: inputName.current.value,
+      email: inputEmail.current.value,
+      password: inputPassword.current.value
+    })
+
+    getUsers()
+
+    inputName.current.value = ""
+    inputEmail.current.value = ""
+    inputPassword.current.value = ""
+    }
+  }
+
+  async function deleteUsers(id: string) {
+    try {
+      await api.delete(`/users/${id}`)
+      getUsers()
+    } catch (error) {
+      console.error("Erro ao deletar usuário", error)
+    }
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <div className="container">
+      <form>
+        <h1>Cadastro de Usuário</h1>
+        <input placeholder="Name :" name="name" type="text" ref={inputName}/>
+        <input placeholder="E-mail :" type="email" name="email" ref={inputEmail}/>
+        <input placeholder="Password :" type="password" name="password" ref={inputPassword}/>
+        <button type="button" onClick={createUsers}>Cadastrar</button>
+      </form>
+
+      {usus.map((usu) => (
+        <div key={usu.id} className="card">
+          <div>
+            <p>Nome : <span>{usu.name}</span> </p>
+            <p>E-mail : <span>{usu.email}</span> </p>
+          </div>
+          <button onClick={() => deleteUsers(usu.id)}>
+            <img src='/assets/lixo.svg' alt="delete" />
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      ))}
     </div>
-  );
+  )
 }
